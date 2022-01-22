76bee83cc976caf43a698b9c8133ea135b773de0
Ruam Pimentel <ruampimentel@gmail.com>
2022-01-22 22:13:40 +0000

    reordering file

--- cohens-d.R
+++ cohens-d.R
@@ -1,12 +1,15 @@
 
+
+# Calculating d from t according to Lakens (2013)
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/
+# Figure 2
+
 # substitute these values 
 t <- 2.57
 n1 <- 43
 n2 <- 43
 
-# Calculating d from t according to Lakens (2013)
-# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/
-# Figure 2
+
 t*( (1/n1)+ (1/n2) )
 
 
@@ -15,4 +18,5 @@ d_from_t <- function(t, n1, n2) {
   t*( (1/n1)+ (1/n2) )
 }
 
+# testing function
 d_from_t(2.57, 43, 43)
