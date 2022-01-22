f7e98fc4a71d116e92890f9f2d1d2fb4ac05a647
Ruam Pimentel <ruampimentel@gmail.com>
2022-01-22 22:11:49 +0000

    adding cohen's d calculation. d from t

---
+++ cohens-d.R
@@ -0,0 +1,18 @@
+
+# substitute these values 
+t <- 2.57
+n1 <- 43
+n2 <- 43
+
+# Calculating d from t according to Lakens (2013)
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/
+# Figure 2
+t*( (1/n1)+ (1/n2) )
+
+
+# function
+d_from_t <- function(t, n1, n2) {
+  t*( (1/n1)+ (1/n2) )
+}
+
+d_from_t(2.57, 43, 43)
