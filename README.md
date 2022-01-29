2d86b5c511a32494b96c77110896c90a572c61e8
Ruam Pimentel <ruampimentel@gmail.com>
2022-01-29 17:22:40 +0000

    Cohen's d updates and ideas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # scrapbook
 A collection of many useful syntax
+
+# future ideas and reminders to work on:
+- [ ] For Cohen's d, add new ways to calculate it. 
+      - resources:
+          - Check Quant II syntax for that
+          - [Lakens (2013)](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/pdf/fpsyg-04-00863.pdf)
--- cohens-d.R
+++ cohens-d.R
@@ -1,22 +1,15 @@
 
-
 # Calculating d from t according to Lakens (2013)
-# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3840331/pdf/fpsyg-04-00863.pdf
 # Figure 2
 
-# substitute these values 
-t <- 2.57
-n1 <- 43
-n2 <- 43
-
-
-t*( (1/n1)+ (1/n2) )
-
-
-# function
-d_from_t <- function(t, n1, n2) {
+# function 
+# Between subject design ---- 
+d_from_t_between <- function(t, n1, n2) {
   t*( (1/n1)+ (1/n2) )
-}
+  }
 
 # testing function
-d_from_t(3.20, 28, 28)
+d_from_t_between(t = 2.24,
+                 n1 = 25,
+                 n2 = 25)
