68f3388ef08365c6899214571ad800e26a06c7ad
Ruam Pimentel <ruampimentel@gmail.com>
2022-02-13 12:15:37 -0500

    cleaning functions

--- rtweet_settings.R
+++ rtweet_settings.R
@@ -1,17 +1,12 @@
 library("rtweet")
 
 ## load rtweet
-creds <- read.csv("C:/Users/ruamp/OneDrive - University of Toledo/R studying/Twitter/data/twitter_creds.csv")
-
-## store api keys (these are fake example values; replace with your own keys)
-app_name <- creds$app_name
-api_key <- creds$api_key
-api_secret_key <- creds$api_secret
-access_token <- creds$access_token
-access_secret <- creds$access_secret
-
+creds <- read.csv("twitter_creds.csv")
+## store api keys 
+list2env(creds, .GlobalEnv)
+rm(creds)
 # authenticate via web browser
-token <- create_token(
-  app = app_name,
-  consumer_key = api_key,
-  consumer_secret = api_secret_key)
+token <- create_token(app_name, api_key, api_secret_key)
+rm(list = ls.str(mode = 'character'))
+
+
