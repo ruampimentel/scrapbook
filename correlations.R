cdf7de9ceef5d9415f12a2c9579df6a3a1578409
Ruam Pimentel <ruampimentel@gmail.com>
2022-02-17 12:57:58 -0500

    new sources

--- correlations.R
+++ correlations.R
@@ -6,3 +6,15 @@ library(PerformanceAnalytics)
 mtcars %>% 
   select(1:3) %>% 
   chart.Correlation(histogram=TRUE, pch=19)
+
+# Correlation graphs
+# http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
+
+M<-cor(mtcars)
+
+corrplot(M, method="circle")
+corrplot(M, method="color", tl.col="black")
+corrplot(M, type="upper")
+
+# correlogram with hclust reordering
+corrplot(M, type="upper", order="hclust")
