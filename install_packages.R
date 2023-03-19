3bc815ec9e80d2eb2d62474761fe367d763add28
Ruam Pimentel <74716041+ruampimentel@users.noreply.github.com>
2023-03-18 22:22:20 -0400

    creating syntax with all packages that I use to easilly reinstall them when updating R and Rstudio

---
+++ install_packages.R
@@ -0,0 +1,16 @@
+
+
+
+name_packages <- c('devtools', 'tidyverse', 'effectsize', 
+                   'DataExplorer', 'usethis', 'PerformanceAnalytics', 
+                   'report', 'lm.beta', 'apaTables', 'psych', 
+                   'GPArotation', 'MplusAutomation', 'grafify', 
+                   'datapasta', 'dplyr', 'tibble', 'readxl', 'janitor', 
+                   'xlsx', 'tidyr', 'ggplot2', 'purrr', 'skimr', 'careless', 
+                   'magrittr', 'correlation', 'stringr', 'lavaan', 
+                   'TAM', 'rstatix', 'foreign', 'combinat', 'huxtable',
+                   'easystats')
+
+name_packages |> unique()
+
+install.packages(name_packages)
