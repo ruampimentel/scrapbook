228094134bc2edc1381967eb8a6116c019edd1b7
Ruam Pimentel <74716041+ruampimentel@users.noreply.github.com>
2023-03-21 17:28:18 -0400

    writexl

--- install_packages.R
+++ install_packages.R
@@ -10,7 +10,7 @@ name_packages <- c('devtools', 'tidyverse', 'effectsize',
                    'magrittr', 'correlation', 'stringr', 'lavaan', 
                    'TAM', 'rstatix', 'foreign', 'combinat', 'huxtable',
                    'easystats', 'EFA.dimensions', 'summarytools',
-                   
+                   'writexl',
                    # Addins
                    'ymlthis',
                    'styler',
