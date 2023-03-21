8ae64ccad361e1772da482323c320882705ec573
Ruam Pimentel <74716041+ruampimentel@users.noreply.github.com>
2023-03-21 11:21:59 -0400

    adding addins

--- install_packages.R
+++ install_packages.R
@@ -9,7 +9,14 @@ name_packages <- c('devtools', 'tidyverse', 'effectsize',
                    'xlsx', 'tidyr', 'ggplot2', 'purrr', 'skimr', 'careless', 
                    'magrittr', 'correlation', 'stringr', 'lavaan', 
                    'TAM', 'rstatix', 'foreign', 'combinat', 'huxtable',
-                   'easystats', 'EFA.dimensions')
+                   'easystats', 'EFA.dimensions',
+                   
+                   # Addins
+                   'ymlthis',
+                   'styler',
+                   'ViewPipeSteps')
+
+
 
 name_packages |> unique()
 
